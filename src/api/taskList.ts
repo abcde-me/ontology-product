aaea8ed2248a78780c0a416605ea70db015fcd7c
dmy <dingmingyuan@cestc.cn>
2025-06-27 10:26:21 +0800

    feat：修改作业列表接口参数类型

--- src/api/taskList.ts
+++ src/api/taskList.ts
@@ -1,8 +1,11 @@
 import UAPI from '@/api';
 
 // 作业列表
-export async function getTaskList(
-  params: any[] | Record<string | number, any> | undefined
-) {
+export async function getTaskList(params: {
+  uid: string | number;
+  job_id: string;
+  page: number;
+  page_size: number;
+}) {
   return await UAPI.RES.taskList({}).get(params).inRegion().do();
 }
