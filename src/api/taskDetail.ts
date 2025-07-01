a1b466fb937e054f289d65cc9b7d3e806915a4df
dmy <dingmingyuan@cestc.cn>
2025-07-01 17:36:35 +0800

    feat：作业详情接口

--- src/api/taskDetail.ts
+++ src/api/taskDetail.ts
@@ -1,6 +1,27 @@
 import UAPI from '@/api';
 
-// 作业列表
-export async function getTaskDetail(task_id: number | string) {
+// 作业详情
+export async function getTaskDetail(task_id: string) {
   return await UAPI.RES.taskDetail({ task_id }).get().inRegion().do();
+}
+
+// 作业详情节点
+export async function getTaskDetailNode(params: {
+  id: string;
+  task_type: string;
+  search_key?: string;
+  page?: number;
+  page_size?: number;
+}) {
+  return await UAPI.RES.taskDetailNode({}).get(params).inRegion().do();
+}
+
+// 作业重跑
+export async function taskRerun(params: { id: string; uid: string }) {
+  return await UAPI.RES.taskRerun({}).post(params).inRegion().do();
+}
+
+// 作业停止
+export async function taskStop(params: { id: string; uid: string }) {
+  return await UAPI.RES.taskStop({}).post(params).inRegion().do();
 }
