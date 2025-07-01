ded9a5472f6cfa272085acd00f0299371321c880
sun.yan <sun.yan@cestc.cn>
2025-07-01 19:49:11 +0800

    feat: 新建工作流联调

--- src/api/workflow.ts
+++ src/api/workflow.ts
@@ -30,27 +30,27 @@ export async function getWorkflowDetail(
   workflow_uuid: string | number
 ): Promise<ApiRes<WorkflowDetailRes>> {
   // TODO: 联调
-  // return await UAPI.RES.workflowDetail({ workflow_uuid }).get().inRegion().do();
-  return Promise.resolve({
-    code: 'Success',
-    message: '请求成功',
-    data: {
-      ds_workflow_id: 3242242,
-      workflow_uuid: 'app-903a7d22-dd9d-4ab9-8748-2659c2dbf1ce',
-      workflow_version: '',
-      workflow_name: '新建工作流',
-      source_path: '',
-      target_path: '',
-      run_cycle: '',
-      create_time: new Date('2025-06-27T11:17:56.399+08:00').getTime(),
-      update_time: '2025-06-27T14:23:24.143+08:00',
-      is_online: 1,
-      user_id: 'f7a41491-0dae-4754-94c1-ee1e8315196b',
-      user_name: 'f7a41491-0dae-4754-94c1-ee1e8315196b'
-    },
-    requestId: '',
-    status: 200
-  });
+  return await UAPI.RES.workflowDetail({ workflow_uuid }).get().inRegion().do();
+  // return Promise.resolve({
+  //   code: 'Success',
+  //   message: '请求成功',
+  //   data: {
+  //     ds_workflow_id: 3242242,
+  //     workflow_uuid: 'app-903a7d22-dd9d-4ab9-8748-2659c2dbf1ce',
+  //     workflow_version: '',
+  //     workflow_name: '新建工作流',
+  //     source_path: '',
+  //     target_path: '',
+  //     run_cycle: '',
+  //     create_time: new Date('2025-06-27T11:17:56.399+08:00').getTime(),
+  //     update_time: '2025-06-27T14:23:24.143+08:00',
+  //     is_online: 1,
+  //     user_id: 'f7a41491-0dae-4754-94c1-ee1e8315196b',
+  //     user_name: 'f7a41491-0dae-4754-94c1-ee1e8315196b'
+  //   },
+  //   requestId: '',
+  //   status: 200
+  // });
 }
 
 // 工作流操作（上下线、运行）
