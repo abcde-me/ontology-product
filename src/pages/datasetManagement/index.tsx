4516c224619e7efe38e87c4b49404fb096ca144c
xuzixin <xuzixin@cestc.com>
2025-07-05 18:07:39 +0800

    修改搜索bug

--- src/pages/datasetManagement/index.tsx
+++ src/pages/datasetManagement/index.tsx
@@ -595,7 +595,11 @@ const DatasetManagement: React.FC = () => {
     console.log('批量导出:', selectedRows);
     // Message.success(`开始导出 ${selectedRowKeys.length} 个数据集...`);
   };
-
+  //清除选中状态函数
+  const handClear=()=>{
+    setSelectedRowKeys([]);
+    setSelectedRows([]);
+  }
   return (
     <div
       style={{
@@ -720,6 +724,7 @@ const DatasetManagement: React.FC = () => {
         onCancel={() => setVisible(false)}
         visible={visible}
         exportdatas={selectedRows}
+        handlClear={handClear}
       />
     </div>
   );
