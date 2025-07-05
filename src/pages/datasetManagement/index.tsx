692156622f40ad02fec2df683f4c513742f80cc5
dmy <dingmingyuan@cestc.cn>
2025-07-05 14:31:26 +0800

    feat：数据集管理状态列逻辑及ui实现

--- src/pages/datasetManagement/index.tsx
+++ src/pages/datasetManagement/index.tsx
@@ -9,7 +9,8 @@ import {
   Modal,
   Message,
   Select,
-  Checkbox
+  Checkbox,
+  Tooltip
 } from '@arco-design/web-react';
 import {
   IconPlus,
@@ -18,7 +19,12 @@ import {
   IconDelete,
   IconDownload,
   IconFilter,
-  IconEmpty
+  IconEmpty,
+  IconLoading,
+  IconCloseCircleFill,
+  IconCheckCircleFill,
+  IconExclamationCircleFill,
+  IconInfoCircle
 } from '@arco-design/web-react/icon';
 import { useHistory } from 'react-router-dom';
 import {
@@ -71,13 +77,30 @@ interface Dataset {
 // 状态显示配置
 const getStatusConfig = (status: string) => {
   const statusMap = {
-    creating: { text: '创建中', color: 'blue' },
-    create_fail: { text: '创建失败', color: 'red' },
-    normal: { text: '正常', color: 'green' },
-    version_generating: { text: '版本生成中', color: 'orange' },
-    version_generating_fail: { text: '版本生成失败', color: 'red' }
+    creating: { text: datasetStatusName.creating },
+    create_fail: { text: datasetStatusName.create_fail },
+    normal: { text: datasetStatusName.normal },
+    version_generating: { text: datasetStatusName.version_generating },
+    version_generating_fail: { text: datasetStatusName.version_generating_fail }
   };
-  return statusMap[status] || { text: status, color: 'gray' };
+  return statusMap[status] || { text: status };
+};
+
+// 获取状态icon
+const getStatusIcon = (status: string) => {
+  return status === datasetStatus.creating ? (
+    <IconLoading style={{ color: '#007DFA', margin: '0 5px 0 0' }} />
+  ) : status === datasetStatus.create_fail ? (
+    <IconCloseCircleFill style={{ color: '#EF4444', margin: '0 5px 0 0' }} />
+  ) : status === datasetStatus.normal ? (
+    <IconCheckCircleFill style={{ color: '#10B981', margin: '0 5px 0 0' }} />
+  ) : status === datasetStatus.version_generating ? (
+    <IconLoading style={{ color: '#007DFA', margin: '0 5px 0 0' }} />
+  ) : (
+    <IconExclamationCircleFill
+      style={{ color: '#F97316', margin: '0 5px 0 0' }}
+    />
+  );
 };
 
 const columns = (
@@ -91,13 +114,22 @@ const columns = (
     dataIndex: 'name',
     width: 200,
     render: (name: string, record: Dataset) => (
-      <Button
-        type="text"
-        className={styles.datasetNameLink}
-        onClick={() => handleGoToDetail(record.id)}
+      <span
+        className={
+          record.status === datasetStatus.create_fail ||
+          record.status === datasetStatus.creating
+            ? styles.datasetNameLink
+            : `${styles.datasetNameLink} ${styles.datasetNameHover}`
+        }
+        onClick={() => {
+          record.status === datasetStatus.create_fail ||
+          record.status === datasetStatus.creating
+            ? null
+            : handleGoToDetail(record.id);
+        }}
       >
         {name}
-      </Button>
+      </span>
     )
   },
   {
@@ -138,21 +170,35 @@ const columns = (
   {
     title: '状态',
     dataIndex: 'status',
-    width: 120,
+    width: 180,
     filterIcon: <IconFilter />,
     filters: [
-      { text: '创建中', value: 'creating' },
-      { text: '创建失败', value: 'create_fail' },
-      { text: '正常', value: 'normal' },
-      { text: '版本生成中', value: 'version_generating' },
-      { text: '版本生成失败', value: 'version_generating_fail' }
+      { text: '创建中', value: datasetStatus.creating },
+      { text: '创建失败', value: datasetStatus.create_fail },
+      { text: '正常', value: datasetStatus.normal },
+      { text: '版本生成中', value: datasetStatus.version_generating },
+      { text: '版本生成失败', value: datasetStatus.version_generating_fail }
     ],
     onFilter: (value: string, record: Dataset) => {
       return record.status === value;
     },
     render: (status: string) => {
       const statusConfig = getStatusConfig(status);
-      return <Tag color={statusConfig.color}>{statusConfig.text}</Tag>;
+      return (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          {getStatusIcon(status)}
+          <span>{statusConfig.text}</span>
+          {status === datasetStatus.version_generating_fail ||
+          status === datasetStatus.create_fail ? (
+            <Tooltip mini content="123456789">
+              <IconInfoCircle style={{ margin: '0 0 0 5px' }} />
+            </Tooltip>
+          ) : null}
+          {status === datasetStatus.version_generating_fail ? (
+            <span className={styles.retryText}>重试</span>
+          ) : null}
+        </div>
+      );
     }
   },
   {
@@ -247,8 +293,9 @@ const columns = (
         </Button> */}
         <Button
           type="text"
-          className={`${styles.actionButton} ${styles.export}`}
+          className={`${styles.actionButton} ${record.status === datasetStatus.normal ? styles.export : styles.disabled}`}
           onClick={() => handleExport(record)}
+          disabled={record.status !== datasetStatus.normal}
         >
           导出
         </Button>
@@ -270,6 +317,24 @@ enum searchFieldType {
   description = 'description'
   // src_model = 'src_model',
   // creator_name = 'creator_name'
+}
+
+// 枚举数据集状态
+enum datasetStatusName {
+  creating = '创建中',
+  create_fail = '创建失败',
+  normal = '正常',
+  version_generating = '版本生成中',
+  version_generating_fail = '版本生成失败'
+}
+
+// 枚举数据集状态名称
+enum datasetStatus {
+  creating = 'creating',
+  create_fail = 'create_fail',
+  normal = 'normal',
+  version_generating = 'version_generating',
+  version_generating_fail = 'version_generating_fail'
 }
 
 const DatasetManagement: React.FC = () => {
