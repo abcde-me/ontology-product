5d1dcc14ec9227cf7b6c7fbe81e275a46ea67fb2
jiangjunjie <jiangjunjie@cestc.cn>
2025-07-16 10:40:24 +0800

    fix：ui

--- src/pages/workflowConfig/workflow/nodes/data-video-parser/panel.tsx
+++ src/pages/workflowConfig/workflow/nodes/data-video-parser/panel.tsx
@@ -96,7 +96,7 @@ const Panel: FC<NodePanelProps<VideoParserNodeType>> = ({ id, data }) => {
             handleFilesChange={handleFilesChange}
           />
         </FormItem>
-        <Split className="my-[16px]" />
+        <Split className="mb-[16px]" />
         <FormItem
           label="字幕与音频校验："
           field="audio_options"
