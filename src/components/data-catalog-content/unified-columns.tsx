3575e444480bd7098347f191bbdbf36ec0474a0a
jiaxin <jiaxin01@camelotchina.com>
2025-07-22 19:34:52 +0800

    fix: UE问题修改

--- src/components/data-catalog-content/unified-columns.tsx
+++ src/components/data-catalog-content/unified-columns.tsx
@@ -212,14 +212,15 @@ const renderActionColumn = (
   handAllReset,
   resetPage
 ) => (
-  <div style={{ display: 'flex', gap: 8 }}>
+  <div style={{ display: 'flex' }}>
     <span
       style={{
-        color: '#165DFF',
+        color: '#007DFA',
         display: 'inline-block',
-        width: '100%',
         textAlign: 'center',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        marginRight: '8px',
+        marginLeft: '4px',
       }}
       onClick={() => handleDownload(record, setVisible, selectedFullPath)}
     >
@@ -227,9 +228,8 @@ const renderActionColumn = (
     </span>
     <span
       style={{
-        color: '#165DFF',
+        color: '#007DFA',
         display: 'inline-block',
-        width: '100%',
         textAlign: 'center',
         cursor: 'pointer'
       }}
@@ -255,7 +255,7 @@ export const getUnifiedColumns = (
   dataType: 'volume' | 'database',
   setVisible,
   hoveredRowId = null,
-  refreshData = () => {}, // 添加刷新数据的回调函数
+  refreshData = () => { }, // 添加刷新数据的回调函数
   selectedKey?: string, // 添加selectedKey参数
   selectedFullPath?: string, // 添加selectedFullPath参数
   customFileTypeFilters?: any[], // 新增参数，用于接收动态生成的文件类型筛选器
