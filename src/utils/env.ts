18a77602031f57823dba0a51d3a3abac9e39da85
jiangjunjie <jiangjunjie@cestc.cn>
2025-07-24 15:32:30 +0800

    fix：util

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,5 @@
-export const isInCCloud = !!(window as any).SERVER_FLAGS?.basePath
+export const isInCCloud = !!(window as any).SERVER_FLAGS?.basePath;
 
-export const isSingleApp = !(window as any).SERVER_FLAGS?.basePath
+export const isSingleApp = !(window as any).SERVER_FLAGS?.basePath;
+
+export const isInFrame = window.self !== window.top;
