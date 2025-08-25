112b63e1a4a0ad63b597a863da914b88c89a90ea
WeiTing <weiting@cestc.cn>
2025-08-25 17:30:44 +0800

    feat: SQL开发 数据集导出任务列表组件

--- src/pages/sql/index.tsx
+++ src/pages/sql/index.tsx
@@ -12,14 +12,15 @@ import DataIcon from '@/assets/python/data-left-menu.svg';
 import SuanziIcon from '@/assets/python/suanzi-left-menu.svg';
 import PythonIcon from '@/assets/python/python-left-menu.svg';
 import './index.scss';
+import Datasets from './components/Datasets';
 
 const { Content, Sider } = Layout;
 const TabPane = Tabs.TabPane;
 
-type TabKey = 'files' | 'tools' | 'data';
+type TabKey = 'data' | 'files' | 'dataset';
 
 export default function SqlEditorIndex() {
-  const [activeTab, setActiveTab] = useState<TabKey>('files');
+  const [activeTab, setActiveTab] = useState<TabKey>('dataset');
   const [currentFileId, setCurrentFileId] = useState<string | null>(null);
 
   const handleTabChange = (key: string) => {
@@ -29,9 +30,23 @@ export default function SqlEditorIndex() {
   const handleFileOpen = (fileId: string) => {
     setCurrentFileId(fileId);
   };
+
+  function getSiderWidth() {
+    switch (activeTab) {
+      case 'data':
+        return 288;
+      case 'files':
+        return 288;
+      case 'dataset':
+        return 46;
+      default:
+        return 288;
+    }
+  }
+
   return (
     <Layout className="notebook-layout">
-      <Sider width={280} className="notebook-sider">
+      <Sider width={getSiderWidth()} className="notebook-sider">
         <Tabs
           activeTab={activeTab}
           onChange={handleTabChange}
@@ -39,22 +54,56 @@ export default function SqlEditorIndex() {
           className="notebook-tabs"
           type="rounded"
         >
-          <TabPane key="files" title={<PythonIcon></PythonIcon>}>
-            <PythonTabContent type="files" onFileOpen={handleFileOpen} />
-          </TabPane>
           <TabPane key="data" title={<DataIcon></DataIcon>}>
             <NotebookTabContent type="data" />
           </TabPane>
-          <TabPane key="tools" title={<SuanziIcon></SuanziIcon>}>
-            <NotebookTabContent type="tools" />
+          <TabPane key="files" title={<PythonIcon></PythonIcon>}>
+            <PythonTabContent type="files" onFileOpen={handleFileOpen} />
           </TabPane>
+          <TabPane key="dataset" title={<SuanziIcon></SuanziIcon>}></TabPane>
         </Tabs>
       </Sider>
       <Content className="notebook-content">
-        <NotebookMainContent currentFileId={currentFileId} />
+        {(activeTab === 'files' || activeTab === 'data') && (
+          <NotebookMainContent currentFileId={currentFileId} />
+        )}
+
+        {activeTab === 'dataset' && <Datasets />}
       </Content>
     </Layout>
   );
+
+  // return (
+  //   <div className="flex h-full overflow-hidden bg-white">
+  //     <div className="w-[300px] shrink-0 border-r">
+  //       <Tabs tabPosition="left" className="my-vertical-tabs h-full">
+  //         <TabPane key="data" title={<DataIcon></DataIcon>}>
+  //           <div className="flex h-full flex-col overflow-hidden">
+  //             <div className="flex-1 overflow-auto">
+  //               {/* 源数据 */}
+  //               <NotebookTabContent type="data" />
+  //             </div>
+  //           </div>
+  //         </TabPane>
+
+  //         <TabPane key="script" title={<PythonIcon></PythonIcon>}>
+  //           <div className="flex h-full flex-col overflow-hidden">
+  //             <div className="flex-1 overflow-auto">
+  //               {/* 脚本列表 */}
+  //               <PythonTabContent type="files" onFileOpen={handleFileOpen} />
+  //             </div>
+  //           </div>
+  //         </TabPane>
+  //       </Tabs>
+  //     </div>
+
+  //     <div className="flex-1">
+  //       {/* SQL工作区 */}
+  //       <SqlWorkspace />
+  //     </div>
+  //   </div>
+  // );
+
   // return (
   //   <div className="flex h-full overflow-hidden bg-white">
   //     <div className="w-[300px] shrink-0 border-r">
