48f344c1bc54c6357f0c3867860d53dca3981b8d
sun.yan <sun.yan@cestc.cn>
2025-08-27 10:21:57 +0800

    fix: bug

--- src/index.tsx
+++ src/index.tsx
@@ -185,7 +185,6 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
 
   // @ts-expect-error
   module.hot.accept('./pages/login', () => {
-    console.log('22222222');
     // 当 Login 组件发生变化时，重新渲染
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const NextLogin = require('./pages/login').default;
