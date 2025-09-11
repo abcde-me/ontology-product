d4ce89b5fb9c02b7d39cea4876e220f221eda3df
jiaxin <jiaxin01@camelotchina.com>
2025-09-11 15:32:32 +0800

    feat: 修改树

--- src/pages/requirement/components/DetailModal.tsx
+++ src/pages/requirement/components/DetailModal.tsx
@@ -162,7 +162,7 @@ const DataSourceModal: React.FC<DataSourceModalProps> = ({
               if (e?.node?.props?.dataRef?.level === 3) {
                 setCurrent(1);
                 setPageSize(10);
-                setCheckedKeys([value[0]?.split(',')?.[0]]);
+                setCheckedKeys([value[0]?.split(',')?.[2]]);
               }
             }}
           />
@@ -174,23 +174,7 @@ const DataSourceModal: React.FC<DataSourceModalProps> = ({
   };
   //格式化时间函数
   const formatDateTime = (dateTimeString: string): string => {
-    if (type === 'detail') {
-      return dayjs(dateTimeString).format('YYYY-MM-DD HH:mm:ss');
-    } else {
-      try {
-        const date = new Date(dateTimeString);
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-      } catch (error) {
-        return dateTimeString; // 如果格式化失败，返回原字符串
-      }
-    }
+    return dayjs(dateTimeString).format('YYYY-MM-DD HH:mm:ss');
   };
   const columns = [
     {
