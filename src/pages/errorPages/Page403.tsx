1eca9c7c8a25afb02c605d3c0a619d6447d7088a
liangchaofei <liangchaofei@cestc.cn>
2025-10-22 19:53:26 +0800

    feat: add 403 page

--- src/pages/errorPages/Page403.tsx
+++ src/pages/errorPages/Page403.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import { Result } from '@arco-design/web-react';
-import { useHistory } from 'react-router-dom';
-
+import { Divider, Result } from '@arco-design/web-react';
+import Nopermission from '@/assets/nopermission.svg';
 /**
  * 403 无权限页面
  */
-export const Page403: React.FC = () => {
-  const history = useHistory();
-
+const Page403: React.FC = () => {
   return (
-    <div className="flex h-full items-center justify-center bg-gray-50">
+    <div className="m-5 flex h-[calc(100%-40px)] items-center justify-center rounded-xl bg-gray-50">
       <div className="text-center">
         <Result
           status="403"
-          title="暂无权限"
+          icon={null}
           subTitle={
-            <div className="space-y-2">
-              <div className="text-lg font-medium text-gray-700">
+            <div>
+              <div style={{ height: '130px' }} className="mb-2">
+                <Nopermission style={{ width: '130px', height: '130px' }} />
+              </div>
+              <div className="text-[20px] font-medium text-[#0F172A]">
+                暂无权限
+              </div>
+              <div className="mb-3 mt-1 text-[14px] font-normal text-[#6E7B8D]">
                 您当前没有被分配到该项目，请联系管理员获取访问权限
               </div>
-              <div className="text-sm text-gray-500">姓名：xxx 电话：xxx</div>
+              <div className="flex items-center justify-center text-[14px] font-normal leading-[22px] text-[var(--color-text-1)]">
+                <div>姓名：张三</div>
+                <Divider type="vertical" className="mx-2 h-[14px]" />
+                <div>电话：13812345678</div>
+              </div>
             </div>
           }
         />
