965ef896c0ed703946293b3a1334ea20564d793d
jiaxin <jiaxin01@camelotchina.com>
2025-11-06 16:51:16 +0800

    fix: bug修复

--- src/pages/dataCatalog/components/editable-tree/useEditableTree.tsx
+++ src/pages/dataCatalog/components/editable-tree/useEditableTree.tsx
@@ -109,10 +109,7 @@ export function useEditableTree({ catalogTreeStore }) {
     const keys: string[] = [];
     const loop = (data: TreeDataType[]) => {
       data?.forEach?.((item) => {
-        if (
-          typeof item.title === 'string' &&
-          item.title?.toLowerCase().indexOf(value.toLowerCase()) > -1
-        ) {
+        if (typeof item.title === 'string' && item.title?.indexOf(value) > -1) {
           if (item.parentKey) {
             keys.push(item.parentKey);
           }
@@ -547,7 +544,7 @@ export function useEditableTree({ catalogTreeStore }) {
 
     let TitleText: ReactNode = title;
     if (searchValue.length && typeof title === 'string') {
-      const index = title.toLowerCase().indexOf(searchValue.toLowerCase());
+      const index = title.indexOf(searchValue);
       if (index !== -1) {
         const prefix = title.slice(0, index);
         const suffix = title.slice(index + searchValue.length);
