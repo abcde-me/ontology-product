6d003883a1e6581b93fb7dcac098dca28b1a05fc
duanfeiyu <duanfeiyu@cestc.cn>
2025-11-12 16:56:33 +0800

    fix: 字段映射

--- src/pages/requirement/hooks/useGetModelInfo.ts
+++ src/pages/requirement/hooks/useGetModelInfo.ts
@@ -50,8 +50,8 @@ export const useGetModelLabelList = (
           return [];
         }
         return res.data.label_list.map((item) => ({
-          label: item.label_name,
-          value: item.label_mapping
+          label: item.label_mapping,
+          value: item.label_shape
         }));
       }
     }
