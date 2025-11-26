0d30004bd36a762c24212656b05480b5043af51d
liangchaofei <liangchaofei@cestc.cn>
2025-11-26 10:27:13 +0800

    feat: 修改type类型

--- src/pages/ragDetail/utils/newTreeData.ts
+++ src/pages/ragDetail/utils/newTreeData.ts
@@ -12,7 +12,7 @@ export const newTreeData: {
   data: {
     catalogs: {
       level: 0,
-      type: 'title',
+      type: 'Title',
       chunk_id: 'title::URbXKJ::0',
       content: '“有为政府”如何促进中国产业政策演进',
       positions: [
@@ -24,7 +24,7 @@ export const newTreeData: {
       children: [
         {
           level: 1,
-          type: 'text',
+          type: 'Text',
           chunk_id: 'segment-e41fdb6d-58e2-4a47-9d43-77eb338bc51d',
           content: '基于移动通信产业的案例分析',
           positions: [
@@ -37,7 +37,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::DEM3sb::2',
           content: '$\\bigcirc$ 张睿涵，王石玉²',
           positions: [
@@ -49,7 +49,7 @@ export const newTreeData: {
           children: [
             {
               level: 1,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-254cae91-f499-481e-ba8e-7a2e4c1690fa',
               content: '（1. 东华大学旭日工商管理学院，上海2',
               positions: [
@@ -64,7 +64,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::rgKNIY::8',
           content: '1 问题提出',
           positions: [
@@ -76,7 +76,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-84896be5-6209-4805-bd27-21e1092996fb',
               content: '国家的发展需“强化国家战略科技力量”，并',
               positions: [
@@ -93,7 +93,7 @@ export const newTreeData: {
             },
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-bf0070e2-f861-4e5a-8c84-81bab042738b',
               content: '本文的数据来源以下三个渠道，以确保对中国',
               positions: [
@@ -108,7 +108,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::CBuSHP::17',
           content: '2 中国移动通信产业政策节奏分析',
           positions: [
@@ -120,7 +120,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-bfa6bd7e-4bad-436a-96ad-1892fa75f752',
               content: '政策目标是政策致力于实现的效果与价值体现',
               positions: [
@@ -139,7 +139,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::MbAkXM::33',
           content: '3 移动通信产业追赶阶段政策分析',
           positions: [
@@ -151,7 +151,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-115b37a6-d360-4412-b22c-bc01ee3cd8b4',
               content: '在1G至3G时代，中国的移动通信产业处于',
               positions: [
@@ -174,7 +174,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::eudgZ8::42',
           content: '4 移动通信产业并行阶段政策分析',
           positions: [
@@ -186,7 +186,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-ef7e101d-de22-4e4f-885a-92ddbdba6c23',
               content: '随着4G时代的到来，中国的移动通信产业开',
               positions: [
@@ -205,7 +205,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::U89CIb::50',
           content: '5 移动通信产业领先阶段政策分析',
           positions: [
@@ -217,7 +217,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-a06fd1bf-7fa6-4f69-a103-5933352b7a25',
               content: '进入5G时代，中国的移动通信产业实现了从',
               positions: [
@@ -234,7 +234,7 @@ export const newTreeData: {
             },
             {
               level: 1,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-cc32f221-604d-4b74-a3b0-364fe34a28ab',
               content: '在个性特征中，移动通信产业政策共现网络表',
               positions: [
@@ -257,7 +257,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::dQKKVp::70',
           content: '6 理论构建：基于案例的整合分析框架',
           positions: [
@@ -270,7 +270,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::yTZPuv::71',
           content: '6.1 “政策目标－政策工具－政策执行”的三维分析框架',
           positions: [
@@ -282,7 +282,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-2bb10623-0298-4607-bc44-062668fca6ab',
               content: '通过案例研究发现，政策目标导向下多种可供',
               positions: [
@@ -297,7 +297,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::2EVhgx::78',
           content: '6.2 理论对话：基于中国情境的产业政策分析与凯恩斯主义的交融',
           positions: [
@@ -309,7 +309,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-5eec0a51-2de3-4abe-a645-c3134b115143',
               content: '在探讨“有为政府”如何促进中国产业政策演',
               positions: [
@@ -328,7 +328,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::9wsQ1u::91',
           content: '7 结论',
           positions: [
@@ -340,7 +340,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-384ec5e1-060b-41e3-9ea0-8ff0d2be3ff3',
               content: '本文基于“有为政府”理论，构建政府促进移',
               positions: [
@@ -359,7 +359,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::7vZRdj::98',
           content: '参考文献：',
           positions: [
@@ -371,7 +371,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-11daf559-5760-4b7c-955b-87d8efbe3947',
               content: '[1] 叶堂林，毛若冲，李国梁.城市群内',
               positions: [
@@ -388,7 +388,7 @@ export const newTreeData: {
             },
             {
               level: 1,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-a858499f-3d09-451c-8004-38e3b7a0be82',
               content: ' 基于有为政府和有效市场互动的企业创新质',
               positions: [
@@ -405,7 +405,7 @@ export const newTreeData: {
             },
             {
               level: 1,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-5c60da5c-4384-454e-a16e-3fad237ff367',
               content: ' 上海经济研究，2022（1）：61- ',
               positions: [
@@ -424,7 +424,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::fkHRyC::105',
           content:
             '(8) How the "Effective Government" Promotes the Evolution of China\'s Industrial Policies ',
@@ -437,7 +437,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-e2f622cf-da59-4e23-9ffa-5a3745db2cb4',
               content: 'A Case Study Based o',
               positions: [
@@ -452,7 +452,7 @@ export const newTreeData: {
         },
         {
           level: 1,
-          type: 'title',
+          type: 'Title',
           chunk_id: 'title::gAloCM::113',
           content: 'Abstract ID: 1672-6162(2024)01-0094-EA ',
           positions: [
@@ -464,7 +464,7 @@ export const newTreeData: {
           children: [
             {
               level: 2,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-43b98288-0416-4ea0-8cdf-4643ea7e2007',
               content: 'Abstract: This artic',
               positions: [
@@ -477,7 +477,7 @@ export const newTreeData: {
             },
             {
               level: 1,
-              type: 'text',
+              type: 'Text',
               chunk_id: 'segment-0117c9b1-01ab-4f1d-8464-837bc19e58b4',
               content: ' It reveals the key ',
               positions: [
