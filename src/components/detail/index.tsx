67ddc5bc2d19ce5a433dc92755c4b9bbbcd3e325
丁明远 <dingmingyuan@cestc.cn>
2025-12-02 16:10:49 +0800

    fix：修改渲染逻辑，防止数据库表类型数据渲染分页组件

--- src/components/detail/index.tsx
+++ src/components/detail/index.tsx
@@ -2002,20 +2002,7 @@ const DatasetDetail = (props: {
                 noDataElement({ description: '暂无数据' })
               )}
             </TabPane>
-          ) : datasetDetail?.storage_type === StorageType.table ? (
-            // 数据库表数据内容
-            <TabPane key="content" title="文件列表">
-              <div className="table-scroll-container">
-                <Table
-                  columns={contentTableColumns}
-                  data={contentTableData}
-                  pagination={false}
-                  rowKey="id"
-                  border={false}
-                />
-              </div>
-            </TabPane>
-          ) : (
+          ) : datasetDetail?.storage_type === StorageType.vector ? (
             <TabPane key="content" title="文件列表">
               {datasetDetail?.storage_type === StorageType.vector && (
                 <Input.Search
@@ -2086,6 +2073,20 @@ const DatasetDetail = (props: {
                   sizeOptions={[10, 20, 50, 100]}
                   showJumper
                   sizeCanChange={true}
+                />
+              </div>
+            </TabPane>
+          ) : (
+            // 数据库表数据内容
+            <TabPane key="content" title="文件列表">
+              <div className="table-scroll-container">
+                <Table
+                  columns={contentTableColumns}
+                  data={contentTableData}
+                  pagination={false}
+                  rowKey="id"
+                  border={false}
+                  noDataElement={noDataElement({ description: '暂无数据' })}
                 />
               </div>
             </TabPane>
