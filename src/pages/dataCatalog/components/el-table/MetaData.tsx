ec9ff3bcc17ca54865b698cb854b29a9c8840941
sun.yan <sun.yan@cestc.cn>
2025-12-04 16:41:24 +0800

    fix: 走查

--- src/pages/dataCatalog/components/el-table/MetaData.tsx
+++ src/pages/dataCatalog/components/el-table/MetaData.tsx
@@ -275,7 +275,7 @@ export default function MetaData() {
             styles['header-container']
           )}
         >
-          <div className="text-[16px] font-bold">数据湖目录({total})</div>
+          <div className="text-[16px] font-bold">数据列表({total})</div>
           <Tooltip content="刷新">
             <Button
               type="outline"
