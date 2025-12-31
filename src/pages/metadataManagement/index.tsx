dc661d432a48b8f252c0afb97527d665da2721f7
丁明远 <dingmingyuan@cestc.cn>
2025-12-31 16:41:36 +0800

    fix：创建物理表弹窗中，保存位置下拉框需要支持搜索

--- src/pages/metadataManagement/index.tsx
+++ src/pages/metadataManagement/index.tsx
@@ -647,6 +647,12 @@ PROPERTIES (
             <Select
               placeholder="请选择数据库"
               className={styles.selectTable}
+              showSearch
+              filterOption={(inputValue, option) =>
+                option.props.children
+                  .toLowerCase()
+                  .indexOf(inputValue.toLowerCase()) >= 0
+              }
               style={{ display: 'flex', alignItems: 'center' }}
               addBefore={
                 <Select
