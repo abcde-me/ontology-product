eeb4627b0b0490e437dbd35486ea47a9e7520061
丁明远 <dingmingyuan@cestc.cn>
2026-01-27 15:28:11 +0800

    feat：新建数据集弹窗添加新建场景分类功能

---
+++ src/components/datasetform/AddSceneForm.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Form, Input, Modal, Select } from '@arco-design/web-react';
+import styles from '@/pages/datasetManagement/index.module.scss';
+
+export default function AddSceneForm({
+  addSceneTypeVisible,
+  sceneTypeForm,
+  setAddSceneTypeVisible,
+  handleAddSceneTypeSubmit,
+  newTagList
+}) {
+  return (
+    <Modal
+      className={styles.addSceneTypeModal}
+      visible={addSceneTypeVisible}
+      onOk={() => sceneTypeForm.submit()}
+      onCancel={() => {
+        sceneTypeForm.resetFields();
+        setAddSceneTypeVisible(false);
+      }}
+      title="新增场景类型"
+    >
+      <Form form={sceneTypeForm} onSubmit={handleAddSceneTypeSubmit}>
+        <Form.Item
+          label="场景分类名称："
+          field="sceneTypeName"
+          rules={[{ required: true, message: '请输入场景类型名称' }]}
+        >
+          <Input placeholder="请输入名称" />
+        </Form.Item>
+        <Form.Item label="场景分类标签：" field="sceneTypeTag">
+          <Select
+            mode="multiple"
+            placeholder="请选择标签"
+            options={newTagList.map((item) => ({
+              label: item.name,
+              value: item.name
+            }))}
+          />
+        </Form.Item>
+        <Form.Item
+          label="描述说明："
+          field="sceneTypeDesc"
+          rules={[{ required: true, message: '请输入描述说明' }]}
+        >
+          <Input.TextArea placeholder="可以描述数据集的用途、特点或其他相关信息" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
